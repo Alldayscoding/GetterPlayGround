ad84ba6521155bb8ae43fadd8a4c6ea6f90b83d3
AlldaysCoding <jason.kim3221@gmail.com>
2023-08-17 18:01:47 +0900

    Initial commit

---
+++ src/main/java/com/getter/getterplay/GetterPlayApplication.java
@@ -0,0 +1,13 @@
+package com.getter.getterplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetterPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetterPlayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/getter/getterplay/controller/MatchingController.java
@@ -0,0 +1,16 @@
+package com.getter.getterplay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/matching")
+public class MatchingController {
+
+    @GetMapping("")
+    public String matchingGET(){
+        return "matching";
+    }
+
+}
